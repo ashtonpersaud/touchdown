f65d3faa5c868e6ab5003908fa5a3cbc404a0262
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-11-16 19:26:08 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,9 +16,18 @@ class touchdownSkill(MycroftSkill):
 
     @intent_handler('touchdown.intent')
     def handle_not_are_you_intent(self, message):
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        ser.flush()
-        ser.write(b"touch")
+        serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        serA.flush()
+        serA.write(b"touch")
+        serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+        serB.flush()
+        serB.write(b"touch")        
+        serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        serC.flush()
+        serC.write(b"touch")
+        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        serD.flush()
+        serD.write(b"touch") 
         time.sleep(1.5)
         self.speak_dialog("Touch down")
       
