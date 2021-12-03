7c3a545c5c92c2651bbc2ff62e41569af42eda30
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-12-03 09:52:22 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,9 +25,9 @@ class touchdownSkill(MycroftSkill):
         serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
         serC.flush()
         serC.write(b"touch")
-        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
-        serD.flush()
-        serD.write(b"touch") 
+        #serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        #serD.flush()
+        #serD.write(b"touch") 
         time.sleep(1.5)
         self.speak_dialog("Touch down by the knights")
 
